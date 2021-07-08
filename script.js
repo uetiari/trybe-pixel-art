8541453bcd7cd42bd0cd43b016e7df00a9a818c5
Ariane Ueti <arianeueti@gmail.com>
2021-07-08 19:56:38 -0300

    Requisito #8 e Correção Erros

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let pixelColor = document.querySelector("#pixel-board");
+const pixelColor = document.querySelector('#pixel-board');
 
-let blackColor = document.getElementById("black");
-let blueColor = document.getElementById("blue");
-let greenColor = document.getElementById("green");
-let yellowColor = document.getElementById("yellow");
+const blackColor = document.getElementById('black');
+const blueColor = document.getElementById('blue');
+const greenColor = document.getElementById('green');
+const yellowColor = document.getElementById('yellow');
 
 function catchColor(event) {
-  let selectColor = document.querySelector(".selected");
+  const selectColor = document.querySelector('.selected');
   selectColor.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
-blackColor.addEventListener("click", catchColor);
-blueColor.addEventListener("click", catchColor);
-greenColor.addEventListener("click", catchColor);
-yellowColor.addEventListener("click", catchColor);
+blackColor.addEventListener('click', catchColor);
+blueColor.addEventListener('click', catchColor);
+greenColor.addEventListener('click', catchColor);
+yellowColor.addEventListener('click', catchColor);
 
-pixelColor.addEventListener("click", changeColor);
+pixelColor.addEventListener('click', changeColor);
 
 function changeColor(event) {
-  let selectColor = document.querySelector(".selected");
+  const selectColor = document.querySelector('.selected');
   event.target.style.backgroundColor = selectColor.id;
 }
